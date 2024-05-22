fed56bbd693937574dbc1115580560d712f77dbb
vp2177 <vp2177@users.noreply.github.com>
2024-05-22 04:08:48 +0100

    TickingClock: actually tick; simple <RootWindow>

--- src/desktop.mjs
+++ src/desktop.mjs
@@ -1,14 +1,24 @@
-import {createElement as e, useState, useMemo} from 'react'
+import {createElement as e, useState, useMemo, useEffect, useInsertionEffect} from 'react'
 
-export function TickingClock({showDate = false}) {
-    //const [now, setNow] = useState(() => new Date())
+export function TickingClock({showDate = false, updateInterval = 2000}) {
     const now = new Date
     const _hour = now.getHours().toString().padStart(2, '0')
     const _min = now.getMinutes().toString().padStart(2, '0')
     const _sec = now.getSeconds().toString().padStart(2, '0')
-    // TODO: memoize
+    // TODO: memoize (if needed)
+    const [n, setN] = useState(0)
+    useEffect(() => {
+        const i = setInterval(() => setN((n) => n+1), updateInterval)
+        return () => clearInterval(i)
+    }, [])
 
     return e('time', null, 
         e('span', {}, `${_hour}:${_min}:${_sec}`)
+    )
+}
+
+export function RootWindow({children}) {
+    return e('div', {className: 'rootWindow'},
+        children
     )
 }
