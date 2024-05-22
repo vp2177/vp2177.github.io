fe3494c79cf30d1163a8a9cc9d807db24af68e5a
vp2177 <vp2177@users.noreply.github.com>
2024-05-22 03:23:57 +0100

    TickingClock: HH:MM:SS

--- src/desktop.mjs
+++ src/desktop.mjs
@@ -3,11 +3,12 @@ import {createElement as e, useState, useMemo} from 'react'
 export function TickingClock({showDate = false}) {
     //const [now, setNow] = useState(() => new Date())
     const now = new Date
-    const hour = now.getHours()
-    const min = now.getMinutes()
-    const sec = now.getSeconds()
+    const _hour = now.getHours().toString().padStart(2, '0')
+    const _min = now.getMinutes().toString().padStart(2, '0')
+    const _sec = now.getSeconds().toString().padStart(2, '0')
+    // TODO: memoize
 
     return e('time', null, 
-        e('span', {}, now.toTimeString())
+        e('span', {}, `${_hour}:${_min}:${_sec}`)
     )
 }
